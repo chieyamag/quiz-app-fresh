4ac57547e849bc0415e5f60e7d54fa22ca88331a
山口知恵 <yamaguchi@adminnoMacBook-Air.local>
2025-04-08 13:29:39 +0900

    switch to adapter-vercel and svelte-kit build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter()
+    adapter: vercel()
   }
 };
 
 export default config;
+
 
